83a4be7fc9134a31a90853ee031ad1c8a5481dc1
Josh Justice <me@codingitwrong.com>
2020-08-28 19:45:18 -0400

    Mock login

--- mirage/config.js
+++ mirage/config.js
@@ -1,22 +1,10 @@
 export default function () {
-  // These comments are here to help you get started. Feel free to delete them.
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
-  */
+  this.post('/oauth/token', () => {
+    return {
+      access_token: 'fake_access_token',
+      token_type: 'bearer',
+      expires_in: 7200,
+      created_at: 1531855327,
+    };
+  });
 }
