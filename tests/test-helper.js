ede807061588370f2fce9644a85121c26301a946
Josh Justice <me@codingitwrong.com>
2020-08-29 12:24:29 -0400

    Fix import order

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,7 @@
-import Application from 'homeschool/app';
-import config from 'homeschool/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import Application from 'homeschool/app';
+import config from 'homeschool/config/environment';
 
 setApplication(Application.create(config.APP));
 
