ef64373fa92bb0e0e56f914d746f26b8280bec24
Josh Justice <me@codingitwrong.com>
2020-09-04 08:05:57 -0400

    Add hash to day model

--- app/routes/days/detail.js
+++ app/routes/days/detail.js
@@ -1,20 +1,23 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default class DaysDetailRoute extends Route {
   model({ day_id }) {
-    return this.store.findRecord('day', day_id, {
-      include: [
-        'studentDays',
-        'studentDays.student',
-        'studentDays.contentDay',
-        'studentDays.contentDay.contents',
-        'studentDays.contentDay.contents.subject',
-        'schedulings',
-        'schedulings.student',
-        'schedulings.content',
-        'schedulings.content.subject',
-      ].join(','),
-      reload: true,
+    return RSVP.hash({
+      day: this.store.findRecord('day', day_id, {
+        include: [
+          'studentDays',
+          'studentDays.student',
+          'studentDays.contentDay',
+          'studentDays.contentDay.contents',
+          'studentDays.contentDay.contents.subject',
+          'schedulings',
+          'schedulings.student',
+          'schedulings.content',
+          'schedulings.content.subject',
+        ].join(','),
+        reload: true,
+      }),
     });
   }
 }
