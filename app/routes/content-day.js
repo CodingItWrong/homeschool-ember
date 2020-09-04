058db8b0f3078e06f438125e00247fe16f78b2a6
Josh Justice <me@codingitwrong.com>
2020-09-04 07:49:20 -0400

    Add breadcrumbs

--- app/routes/content-day.js
+++ app/routes/content-day.js
@@ -3,7 +3,12 @@ import Route from '@ember/routing/route';
 export default class ContentDayRoute extends Route {
   model({ content_day_id }) {
     return this.store.findRecord('contentDay', content_day_id, {
-      include: ['contentWeek', 'contents', 'contents.subject'].join(','),
+      include: [
+        'contentWeek',
+        'contentWeek.contentYear',
+        'contents',
+        'contents.subject',
+      ].join(','),
       reload: true,
     });
   }
--- app/routes/content-week.js
+++ app/routes/content-week.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default class ContentWeekRoute extends Route {
   model({ content_week_id }) {
     return this.store.findRecord('contentWeek', content_week_id, {
-      include: ['contentDays'].join(','),
+      include: ['contentYear', 'contentDays'].join(','),
       reload: true,
     });
   }
