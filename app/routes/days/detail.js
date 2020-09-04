a8022dc8b57350e7ec318510a20c2e05507bd878
Josh Justice <me@codingitwrong.com>
2020-09-04 07:00:37 -0400

    WIP: display content day

--- app/routes/days/detail.js
+++ app/routes/days/detail.js
@@ -4,6 +4,11 @@ export default class DaysDetailRoute extends Route {
   model({ day_id }) {
     return this.store.findRecord('day', day_id, {
       include: [
+        'studentDays',
+        'studentDays.student',
+        'studentDays.contentDay',
+        'studentDays.contentDay.contents',
+        'studentDays.contentDay.contents.subject',
         'schedulings',
         'schedulings.student',
         'schedulings.content',
--- mirage/models/day.js
+++ mirage/models/day.js
@@ -2,4 +2,5 @@ import { Model, hasMany } from 'ember-cli-mirage';
 
 export default Model.extend({
   schedulings: hasMany('scheduling'),
+  studentDays: hasMany('studentDay'),
 });
